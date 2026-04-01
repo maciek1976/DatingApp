22a8547ff84a5763ba6efb652efcae5f1bca55a7
Maciej Lelontko <malelontko@gmail.com>
2026-04-01 14:21:04 +0200

    Add Angular project

--- API/Program.cs
+++ API/Program.cs
@@ -8,10 +8,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
