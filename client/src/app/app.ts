efe7a6952976eec9d725e73d54b7582fc7679c3f
Maciej Lelontko <malelontko@gmail.com>
2026-04-08 11:05:29 +0200

    End of section 5

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -2,26 +2,37 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import { Nav } from "../layout/nav/nav";
+import { AccountService } from '../core/services/account-service';
+import { Home } from "../features/home/home";
+import { User } from '../types/user';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav],
+  imports: [Nav, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit{
+export class App implements OnInit {
+  private accountService = inject(AccountService);
   private http = inject(HttpClient);
   protected readonly title = 'Dating app';
-  protected members = signal<any>([]);
+  protected members = signal<User[]>([]);
 
   async ngOnInit() {
     this.members.set(await this.getMembers());
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const user = localStorage.getItem('user');
+    if (!user) return;
+    this.accountService.currentUser.set(JSON.parse(user));
   }
 
   async getMembers() {
     try {
-      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
-    } catch(error) {
+      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
+    } catch (error) {
       console.log(error);
       throw error;
     }
--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../../core/services/account-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
-export class Nav {}
+export class Nav {
+  protected accountService = inject(AccountService);
+  protected creds: any = {}
+
+  login() {
+    this.accountService.login(this.creds).subscribe({
+      next: response => {
+        console.log(response);
+        this.creds = {};
+      },
+      error: error => alert(error.message)
+    });
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
+}
