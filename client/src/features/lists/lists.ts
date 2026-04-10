1ed3dd23e86fd1f540b196f21baaddbec339e38c
Maciej Lelontko <malelontko@gmail.com>
2026-04-10 15:32:06 +0200

    Start of Section 9

--- client/src/features/lists/lists.ts
+++ client/src/features/lists/lists.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MemberService } from '../../core/services/member-service';
+import { Observable } from 'rxjs';
+import { Member } from '../../types/member';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-lists',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './lists.html',
   styleUrl: './lists.css',
 })
-export class Lists {}
+export class Lists {
+  private memberService = inject(MemberService);
+  protected members$: Observable<Member[]>; 
+
+  constructor() {
+    this.members$ = this.memberService.getMembers();
+  }
+}
